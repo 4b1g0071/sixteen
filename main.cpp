8fb35b58ef0223a3640f1c642ad040b7225632b8
user <user@DESKTOP-GON99JR>
2023-04-16 13:16:49 +0800

    加入專案檔案。

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string str1, str2;
+    getline(cin, str1);
+    getline(cin, str2);
+    int i = 0, count = 0;
+    while (true)
+    {
+        i = str2.find(str1, i);
+        if (i != string::npos)
+        {
+            i++;
+            count++;
+        }
+        else
+            break;
+    }
+    cout << count << endl;
+    return 0;
+}
